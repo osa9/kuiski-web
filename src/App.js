59f79aa071a466847a5ad85a6f949bf425726140
osanine <osanine9@gmail.com>
2018-02-04 20:32:13 +0900

    CI test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to KUISki</h1>
         </header>
         <p className="App-intro">
-          KUISki
+          CI Test
         </p>
       </div>
     );
